f5026574d4314d7885de1746661b123d8dffd1ef
elion-Tech <igweokwuekene@gmail.com>
2026-01-07 18:31:11 +0100

    Update controllers.ts

--- controllers.ts
+++ controllers.ts
@@ -5,7 +5,9 @@ import nodemailer from 'nodemailer';
 import ResetToken from './resetTokenModel';
 
 const transporter = nodemailer.createTransport({
-    service: 'gmail', 
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
