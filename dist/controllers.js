c4b82a812f0ad19ce46b9ce8297abc48755f970b
elion-Tech <igweokwuekene@gmail.com>
2026-01-08 08:33:16 +0100

    Update controllers.js

--- dist/controllers.js
+++ dist/controllers.js
@@ -11,8 +11,8 @@ const nodemailer_1 = __importDefault(require("nodemailer"));
 const resetTokenModel_1 = __importDefault(require("./resetTokenModel"));
 const transporter = nodemailer_1.default.createTransport({
     host: 'smtp.gmail.com',
-    port: 465,
-    secure: true,
+    port: 587,
+    secure: false,
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS,
