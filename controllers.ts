df3b0faad966a3700f66ab2f75d683030f98f5cc
elion-Tech <214651729+elion-Tech@users.noreply.github.com>
2026-06-18 11:38:32 +0100

    Update controllers.ts

--- controllers.ts
+++ controllers.ts
@@ -192,6 +192,7 @@ export const CourseController = {
       console.error("Course.getAllLiveSessions error:", e);
       res.status(500).json({ error: "Failed to fetch all live sessions" });
     }
+  },
   }
 };
 
